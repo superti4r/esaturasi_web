ebeee97d04f7ea9afba4cdf4086379a08e82e7d5
Bachtiar Dwi Pramudi <142956484+superti4r@users.noreply.github.com>
2024-10-23 09:33:38 +0700

    [update] fix logo

--- page/guru/home.php
+++ page/guru/home.php
@@ -33,8 +33,8 @@ $foto=$_SESSION['foto_profil_guru'];
                 <div class="nk-sidebar-element nk-sidebar-head">
                     <div class="nk-sidebar-brand">
                         <a href="html/index.html" class="logo-link nk-sidebar-logo">
-                            <img class="logo-light logo-img" src="./images/icon.png" srcset="./images/logo2x.png 2x" alt="logo">
-                            <img class="logo-dark logo-img" src="./images/icon.png" srcset="./images/logo.png 2x" alt="logo-dark">
+                            <img class="logo-light logo-img" src="./images/logo.png" srcset="./images/logo2x.png 2x" alt="logo">
+                            <img class="logo-dark logo-img" src="./images/logo-dark.png" srcset="./images/logo-dark2x.png 2x" alt="logo-dark">
                             <img class="logo-small logo-img logo-img-small" src="./images/logo-small.png" srcset="./images/logo-small2x.png 2x" alt="logo-small">
                         </a>
                     </div>
