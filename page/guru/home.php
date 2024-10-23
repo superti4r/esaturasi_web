878d81620c0a8027aafefb49ec45b193759c4191
Bachtiar Dwi Pramudi <142956484+superti4r@users.noreply.github.com>
2024-10-23 15:48:41 +0700

    [update][fix] resize tampilan

--- page/guru/home.php
+++ page/guru/home.php
@@ -101,56 +101,6 @@ $foto=$_SESSION['foto_profil_guru'];
                                     <img class="logo-dark logo-img" src="./images/icon.png" srcset="./images/icon.png 2x" alt="logo-dark">
                                 </a>
                             </div><!-- .nk-header-brand -->
-                            <div class="nk-header-search ms-3 ms-xl-0">
-                                <em class="icon ni ni-search"></em>
-                                <input type="text" class="form-control border-transparent form-focus-none" placeholder="Cari...">
-                            </div>
-                            <div class="nk-header-tools">
-                                    <li class="dropdown user-dropdown">
-                                        <a href="#" class="dropdown-toggle me-n1" data-bs-toggle="dropdown">
-                                            <div class="user-toggle">
-                                            <div class="user-avatar">
-                                                    <?php if (!empty($foto)): ?>
-                                                        <img src="<?php echo $foto ?>" alt="User Avatar" style="width: 40px; height: 40px; border-radius: 50%;">
-                                                    <?php else: ?>
-                                                        <span>#</span> <!-- Placeholder jika tidak ada foto -->
-                                                    <?php endif; ?>
-                                                </div>
-                                                <div class="user-info d-none d-xl-block">
-                                                    <div class="user-name dropdown-indicator"><?php echo $nama?></div>
-                                                </div>
-                                            </div>
-                                        </a>
-                                        <div class="dropdown-menu dropdown-menu-md dropdown-menu-end">
-                                            <div class="dropdown-inner user-card-wrap bg-lighter d-none d-md-block">
-                                                <div class="user-card">
-                                                <div class="user-avatar">
-                                                    <?php if (!empty($foto)): ?>
-                                                        <img src="<?php echo $foto ?>" alt="User Avatar" style="width: 40px; height: 40px; border-radius: 50%;">
-                                                    <?php else: ?>
-                                                        <span>#</span> <!-- Placeholder jika tidak ada foto -->
-                                                    <?php endif; ?>
-                                                </div>
-                                                    <div class="user-info">
-                                                        <span class="lead-text"><?php echo $nama?></span>
-                                                        <span class="sub-text"><?php echo $email?></span>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="dropdown-inner">
-                                                <ul class="link-list">
-                                                    <li><a class="dark-switch" href="#"><em class="icon ni ni-moon"></em><span>Mode Gelap</span></a></li>
-                                                </ul>
-                                            </div>
-                                            <div class="dropdown-inner">
-                                                <ul class="link-list">
-                                                    <li><a href="logout.php"><em class="icon ni ni-signout"></em><span>Keluar</span></a></li>
-                                                </ul>
-                                            </div>
-                                        </div>
-                                    </li>
-                                </ul>
-                            </div>
                         </div><!-- .nk-header-wrap -->
                     </div><!-- .container-fliud -->
                 </div>
