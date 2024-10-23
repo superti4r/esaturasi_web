d90190df2770e7b2d434dc617720bde3609f10df
Bachtiar Dwi Pramudi <142956484+superti4r@users.noreply.github.com>
2024-10-23 22:18:30 +0700

    [update] fix home Admin

--- page/admin/home.php
+++ page/admin/home.php
@@ -47,9 +47,18 @@ $foto=$_SESSION['foto_profil_guru'];
                     <div class="nk-sidebar-content">
                         <div class="nk-sidebar-menu" data-simplebar>
                             <ul class="nk-menu">
+                            <li class="nk-menu-heading">
+                                    <h6 class="overline-title text-primary-alt">Dashboard</h6>
+                                </li><!-- .nk-menu-item -->
+                                <li class="nk-menu-item">
+                                    <a href="./home.php" class="nk-menu-link">
+                                        <span class="nk-menu-icon"><em class="icon ni ni-home"></em></span>
+                                        <span class="nk-menu-text">Beranda</span>
+                                    </a>
+                                </li>
                                 </li>
                                 <li class="nk-menu-heading">
-                                    <h6 class="overline-title text-primary-alt">Fitur</h6>
+                                    <h6 class="overline-title text-primary-alt">Master</h6>
                                 </li>
                                 <li class="nk-menu-item has-sub">
                                     <a href="#" class="nk-menu-link nk-menu-toggle">
@@ -68,7 +77,7 @@ $foto=$_SESSION['foto_profil_guru'];
                                 <li class="nk-menu-item has-sub">
                                     <a href="#" class="nk-menu-link nk-menu-toggle">
                                         <span class="nk-menu-icon"><em class="icon ni ni-grid-alt-fill"></em></span>
-                                        <span class="nk-menu-text">Pengaturan</span>
+                                        <span class="nk-menu-text">Data</span>
                                     </a>
                                     <ul class="nk-menu-sub">
                                         <li class="nk-menu-item">
@@ -84,6 +93,15 @@ $foto=$_SESSION['foto_profil_guru'];
                                             <a href="../admin/jadwal.php" class="nk-menu-link"><span class="nk-menu-text">Data Jadwal</span></a>
                                     </li>
                                 </ul>
+                                <li class="nk-menu-heading">
+                                    <h6 class="overline-title text-primary-alt">Session</h6>
+                                </li><!-- .nk-menu-item -->
+                                <li class="nk-menu-item">
+                                    <a href="./logout.php" class="nk-menu-link">
+                                        <span class="nk-menu-icon"><em class="icon ni ni-signout"></em></span>
+                                        <span class="nk-menu-text">Log Out</span>
+                                    </a>
+                                </li>
                             </li>
                          </div>
                     </div>
@@ -105,56 +123,6 @@ $foto=$_SESSION['foto_profil_guru'];
                                     <img class="logo-dark logo-img" src="./images/logo-dark.png" srcset="./images/logo-dark2x.png 2x" alt="logo-dark">
                                 </a>
                             </div><!-- .nk-header-brand -->
-                            <div class="nk-header-search ms-3 ms-xl-0">
-                                <em class="icon ni ni-search"></em>
-                                <input type="text" class="form-control border-transparent form-focus-none" placeholder="Cari...">
-                            </div>
-                            <div class="nk-header-tools">
-                                    <li class="dropdown user-dropdown">
-                                        <a href="#" class="dropdown-toggle me-n1" data-bs-toggle="dropdown">
-                                            <div class="user-toggle">
-                                            <div class="user-avatar">
-                                                    <?php if (!empty($foto)): ?>
-                                                        <img src="<?php echo $foto ?>" alt="User Avatar" style="width: 40px; height: 40px; border-radius: 50%;">
-                                                    <?php else: ?>
-                                                        <span>#</span> <!-- Placeholder jika tidak ada foto -->
-                                                    <?php endif; ?>
-                                                </div>
-                                                <div class="user-info d-none d-xl-block">
-                                                    <div class="user-name dropdown-indicator"><?php echo $_SESSION['nama_guru']?></div>
-                                                </div>
-                                            </div>
-                                        </a>
-                                        <div class="dropdown-menu dropdown-menu-md dropdown-menu-end">
-                                            <div class="dropdown-inner user-card-wrap bg-lighter d-none d-md-block">
-                                                <div class="user-card">
-                                                <div class="user-avatar">
-                                                    <?php if (!empty($foto)): ?>
-                                                        <img src="<?php echo $foto ?>" alt="User Avatar" style="width: 40px; height: 40px; border-radius: 50%;">
-                                                    <?php else: ?>
-                                                        <span>#</span> <!-- Placeholder jika tidak ada foto -->
-                                                    <?php endif; ?>
-                                                </div>
-                                                    <div class="user-info">
-                                                        <span class="lead-text"><?php echo $_SESSION['nama_guru']?></span>
-                                                        <span class="sub-text"><?php echo $_SESSION['email_guru']?></span>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="dropdown-inner">
-                                                <ul class="link-list">
-                                                    <li><a class="dark-switch" href="#"><em class="icon ni ni-moon"></em><span>Mode Gelap</span></a></li>
-                                                </ul>
-                                            </div>
-                                            <div class="dropdown-inner">
-                                                <ul class="link-list">
-                                                    <li><a href="logout.php"><em class="icon ni ni-signout"></em><span>Keluar</span></a></li>
-                                                </ul>
-                                            </div>
-                                        </div>
-                                    </li>
-                                </ul>
-                            </div>
                         </div><!-- .nk-header-wrap -->
                     </div><!-- .container-fliud -->
                 </div>
@@ -169,14 +137,6 @@ $foto=$_SESSION['foto_profil_guru'];
                                         <div class="nk-block-head-content">
                                             <h3 class="nk-block-title page-title">Dashboard</h3>
                                         </div><!-- .nk-block-head-content -->
-                                        <div class="nk-block-head-content">
-                                            <div class="toggle-wrap nk-block-tools-toggle">
-                                                <a href="#" class="btn btn-icon btn-trigger toggle-expand me-n1" data-target="pageMenu"><em class="icon ni ni-more-v"></em></a>
-                                                <div class="toggle-expand-content" data-content="pageMenu">
-                                                    <ul class="nk-block-tools g-3">
-                                                        <li>
-                                                 </div>
-                                            </div>
                                         </div><!-- .nk-block-head-content -->
                                     </div><!-- .nk-block-between -->
                                 </div><!-- .nk-block-head -->
